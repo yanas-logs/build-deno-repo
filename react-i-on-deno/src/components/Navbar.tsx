020a05cf8dc7f674c25110125fd6efbf7df287aa
yanas-logs <yanas.dev404@gmail.com>
2025-12-19 00:09:09 +0700

    chore: implement navbar component

--- react-i-on-deno/src/components/Navbar.tsx
+++ react-i-on-deno/src/components/Navbar.tsx
@@ -1,35 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./Navbar.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function Navbar() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <span className="brand">Vite + React</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <ul className="navbar-right">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Docs</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
-
-export default App;
